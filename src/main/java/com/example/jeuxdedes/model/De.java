9f2363fa99b197c6c84931401ce121e3d3d50eac
Abdessamad <abdssamad89rahhaoui@gmail.com>
2022-11-03 19:04:13 +0100

    generation de squelette de l'App

---
+++ src/main/java/com/example/jeuxdedes/model/De.java
@@ -0,0 +1,28 @@
+package com.example.jeuxdedes.model;
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class De {
+
+    /**
+     * Default constructor
+     */
+    public De() {
+    }
+
+    /**
+     * 
+     */
+    private Integer valeur;
+
+    /**
+     * 
+     */
+    public Integer getValeur() {
+        return 0;
+    }
+
+}
---
+++ src/main/java/com/example/jeuxdedes/model/JeuDeDes.java
@@ -0,0 +1,47 @@
+package com.example.jeuxdedes.model;
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class JeuDeDes {
+
+    /**
+     * Default constructor
+     */
+    private JeuDeDes() {
+    }
+
+    /**
+     * 
+     */
+    private Boolean resultat;
+
+    /**
+     * 
+     */
+    private String nom;
+
+    /**
+     * 
+     */
+    public void lancer() {
+        // TODO implement here
+    }
+
+    /**
+     * @param  d
+     */
+    public void getValeur(De d) {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void resultat() {
+        // TODO implement here
+    }
+
+}
